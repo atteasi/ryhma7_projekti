f685825d3f3d86e46f1e65e8fc7aae00d2ac202b
atteasi <atte.asikainen@metropolia.fi>
2022-02-16 16:59:26 +0200

    alustava kartta, aloitus säätietojen hakemiseen

--- js/weather_data.js
+++ js/weather_data.js
@@ -1,8 +1,9 @@
 'use strict'
-
+//A Key for the Weather API
 const key = 'aeeb6bc394d495acdd9b6b0eb06f5e40';
 
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([60.31181787547431, 25.030374526977543], 13);
+
 //Sets up the map
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -12,12 +13,12 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     zoomOffset: -1,
     accessToken: 'pk.eyJ1IjoiYXR0ZWFzaSIsImEiOiJja3pwbmpjYncwMGlwMm9vOTY1MTA0azZ3In0.wHV0EGo-SSYYuE30yNTkAg'
 }).addTo(map);
+
 //Displays the coordinates from where you click
 map.on('click', function (click){
-    const koordinaatit = click.coords;
     const cord = {
-        long: koordinaatit.longitude.toString(),
-        lat: koordinaatit.latitude.toString()
+        long: click.latlng.lng,
+        lat: click.latlng.lat
     }
     console.log(cord);
 });
