e7dfc2ea031ea16afa3a50ffe2937438b17a9c59
atteasi <atte.asikainen@metropolia.fi>
2022-02-18 15:02:08 +0200

    little fixes all around

--- js/weather_data.js
+++ js/weather_data.js
@@ -1,13 +1,24 @@
 'use strict'
 //A Key for the Weather API
 const key = 'aeeb6bc394d495acdd9b6b0eb06f5e40';
-
+//Setting up the map that is used by our web page
 const map = L.map('map').setView([60.31181787547431, 25.030374526977543], 13);
 
+const infoja = document.querySelector('article');
+
+const kartta = document.querySelector('#map');
 //Sets up the map
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
+    minZoom: 3,
+    setZoom: 18,
+    latLngBounds: [
+        //south west
+        [40.712, -74.227],
+        //north east
+        [40.774, -74.125]
+    ],
     id: 'mapbox/streets-v11',
     tileSize: 512,
     zoomOffset: -1,
@@ -23,18 +34,29 @@ map.on('click', function (click) {
     haeSaaTiedot(cord);
 });
 
+//Function that fetches the Current Weather Data from the OpenWeatherMap API
 function haeSaaTiedot(cord) {
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${cord.lat}&lon=${cord.long}&appid=${key}&lang=fi&units=metric`
     fetch(url)
         .then(function (rsp) {
             return rsp.json();
         }).then(function (tiedot) {
-            console.log(tiedot);
-            listaaSaaTietoja(tiedot);
+        console.log(tiedot);
+        listaaSaaTietoja(tiedot);
     });
 }
+
 //Logs the weather info into console
-function listaaSaaTietoja(info){
+function listaaSaaTietoja(info) {
+    kartta.style.width = '70%';
+    infoja.classList.replace('hidden', 'visible');
+    document.getElementById('kaupunki').innerHTML = info.name;
+    document.getElementById('lampo').innerHTML = `Lämpötila: ${info.main.temp}C°`;
+    document.getElementById('desc').innerHTML = `Tämän hetken säätilanne: ${info.weather[0].description}`
+
+
+
+
     //The Country you Clicked on
     console.log(info.sys.country);
     //The City you Clicked on
@@ -47,4 +69,11 @@ function listaaSaaTietoja(info){
     console.log(`Tuulen nopeus: ${info.wind.speed}m/s`);
     //The Current Weather Condition
     console.log(info.weather[0].description);
+    map.flyTo([info.coord.lat, info.coord.lon], 11);
 }
+
+document.getElementById('nappi').addEventListener('click', function (){
+   kartta.style.width = '100%';
+   infoja.classList.replace('visible', 'hidden');
+
+});
